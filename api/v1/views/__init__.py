eb82f6a9757b982a706630f9fa439aa636eccc71
Stosh09 <stoshochieng@gmail.com>
2024-01-09 02:25:21 +0300

    Added file path in index.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,13 +4,13 @@
 from flask import Blueprint
 
 # create a variable app_views which is an instance of Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1/views')
 
 # wildcard import of everything in the package api.v1.views.index
-#from api.v1.views.index import *
-#from api.v1.views.cities import *
-#from api.v1.views.amenities import *
-#from api.v1.views.users import *
-#from api.v1.views.places import *
-#from api.v1.views.places_reviews import *
-#from api.v1.views.places_amenities import *
+from api.v1.views.index import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
