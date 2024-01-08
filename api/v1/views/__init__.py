7bff250f141c3d825b465173c0c86c964876238e
Stosh09 <stoshochieng@gmail.com>
2024-01-09 00:00:44 +0300

    Set up APIs

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" __init__ file for api/v1/views folder"""
+
+from flask import Blueprint
+
+# create a variable app_views which is an instance of Blueprint
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
